bd2dc9f04fbedd9b6d9400ed014b5e2100c411cc
Sang Nguyễn Ngọc <smt.ngocsang@gmail.com>
2023-08-28 16:56:19 +0700

    Add project files.

---
+++ eShop.Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using eShop.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ShipEmail).IsRequired().IsUnicode(false).HasMaxLength(50);
+        }
+    }
+}
